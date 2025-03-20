a29cad8dd55271babc5dce221253d05822bc252e
ulaskrsln <ulas.karaaslan@icloud.com>
2025-03-20 17:14:29 +0300

    CSharpEğitimim

---
+++ ProjectExamples/11_LoginApp/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//DosyalarıYule()
+//Kullanıcıadi()
+//Sifre()
+//DosyayyKaydet()
+
+
+namespace _11_LoginApp
+{
+   
+    class Program
+    {
+        static string dosyayolu = "kullanicilar.txt";   //ullanıcıları tutcağımız dosya
+        static List<string> kullanicilar = new List<string>();  //Kullanıcıları tutan liste
+        static void Main(string[] args)
+        {
+            DosyalarıYukle(); // Program başlarken dosyadan kullanıcıları yükle
+            bool i = true;
+
+            while (i == true)
+            {
+                Console.WriteLine("\n--- GİRİŞ EKRANI ---");
+                Console.WriteLine("1 - Kullanıcı Ekle");
+                Console.WriteLine("2 - Giriş Yap");
+                Console.WriteLine("3 - Çıkış");
+                Console.Write("Seçiminizi yapın: ");
+                string secim = Console.ReadLine();
+                switch (secim)
+                {
+                    case "1":
+                        KullaniciEkle();
+                        break;
+                    case "2":
+                        GirisYap();
+                        i=false;
+
+                        break;
+                    case "3":
+                        DosyayiKaydet(); // Çıkarken kullanıcıları kaydet
+                        Console.WriteLine("Kullanıcılar kaydedildi. Programdan çıkılıyor...");
+                        return;
+                    default:
+                        Console.WriteLine("Geçersiz seçim! Tekrar deneyin.");
+                        break;
+                }
+            }
+        }
+
+
+        static void KullaniciEkle()
+        {
+            Console.Write("Eklemek istediğiniz kullanıcı adını yazın: ");
+            string kullaniciAdi = Console.ReadLine();
+            Console.Write("Eklemek istediğiniz şifreyi yazın: ");
+            string sifre = Console.ReadLine();
+            kullanicilar.Add(kullaniciAdi + " " + sifre);
+            Console.WriteLine("Kullanıcı başarıyla eklendi!");
+        }
+
+        static void GirisYap()
+        {
+            Console.Write("Kullanıcı adınızı girin: ");
+            string kullaniciAdi = Console.ReadLine();
+            Console.Write("Şifrenizi girin: ");
+            string sifre = Console.ReadLine();
+            if (kullanicilar.Contains(kullaniciAdi + " " + sifre))
+            {
+                Console.WriteLine("Giriş başarılı! Hoş geldiniz.");
+            }
+            else
+            {
+                Console.WriteLine("Kullanıcı adı veya şifre hatalı!");
+            }
+        }
+
+        static void DosyalarıYukle()
+        {
+            if (File.Exists(dosyayolu))
+            {
+                kullanicilar = File.ReadAllLines(dosyayolu).ToList();
+                Console.WriteLine("Kullanıcılar yüklendi.");
+            }
+            else
+            {
+                Console.WriteLine("Kullanıcılar dosyası bulunamadı. Yeni bir dosya oluşturulacak.");
+                File.Create(dosyayolu).Close();
+            }
+        }
+
+        static void DosyayiKaydet()
+        {
+            File.WriteAllLines(dosyayolu, kullanicilar);
+            Console.WriteLine("Kullanıcılar dosyası kaydedildi.");
+        }
+
+    }
+}
---
+++ ProjectExamples/12_GPA_Calculator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_GPA_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Not Hesaplama Sistemine Hoşgeldiniz.");
+            Console.WriteLine("(2 vize(%30+%30) + 1 final(%40) şeklinde hesaplama yapılacak.)");
+            Console.Write("Lütfen ilk vize notunuzu giriniz:");
+            double vize1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Lütfen ikinci vize notunuzu giriniz:");
+            double vize2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Lütfen final notunuzu giriniz:");
+            double final = Convert.ToDouble(Console.ReadLine());
+
+            double GPA = (vize1 * 0.3) + (vize2 * 0.3) + (final * 0.4);
+
+            Console.WriteLine("Not Ortalamanız: " + GPA);
+
+            if (GPA >= 90)
+            {
+                Console.WriteLine("Harf Notunuz: AA");
+            }
+            else if (GPA >= 80)
+            {
+                Console.WriteLine("Harf Notunuz: BA");
+            }
+            else if (GPA >= 70)
+            {
+                Console.WriteLine("Harf Notunuz: BB");
+            }
+            else if (GPA >= 60)
+            {
+                Console.WriteLine("Harf Notunuz: CB");
+            }
+            else if (GPA >= 55)
+            {
+                Console.WriteLine("Harf Notunuz: CC");
+            }
+            else if (GPA >= 50)
+            {
+                Console.WriteLine("Harf Notunuz: DC");
+            }
+            else if (GPA >= 40)
+            {
+                Console.WriteLine("Harf Notunuz: DD");
+            }
+            else if (GPA >= 25)
+            {
+                Console.WriteLine("Harf Notunuz: FD");
+            }
+            else
+            {
+                Console.WriteLine("Harf Notunuz: FF");
+            }
+        }
+    }
+}
